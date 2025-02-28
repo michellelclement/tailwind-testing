4a5e7148668fca262942ac159310417f30598f70
Michelle Clement <65297933+michellelclement@users.noreply.github.com>
2025-02-28 11:00:36 +0000

    Tidy

---
+++ README.md
@@ -0,0 +1,1 @@
+# Super simple starter file
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// import "./styles/style.css";
+// import javascriptLogo from "./javascript.svg";
+// import viteLogo from "/vite.svg";
+// import { setupCounter } from "./counter.js";
+
+// document.querySelector("#app").innerHTML = console.log("Hello from Javascipt");
+
+// setupCounter(document.querySelector("#counter"));
